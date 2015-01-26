fbfc1fb6afb9fd8b8d1cc67c6bc968bfcd0814cb
André Alçada Padez <andre.padez@gmail.com>
2015-01-26 21:46:19 +0100

    asyncrhronicity

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
 var express = require('express');
 var app = express();
 var port = 8080;
+
 
 app.get('/', function(req, res){
 
@@ -10,7 +10,14 @@ app.get('/', function(req, res){
 });
 
 console.log('before listen');
-app.listen(port, function(){
-    console.log('now i\'m listening on port ' + port);
-});
+app.listen(port, whenGet);
 console.log('after listen');
+
+
+function whenGet(){
+    console.log('now i\'m listening on port ' + port);
+}
+
+setTimeout(function(){console.log(1)}, 1000);
+setTimeout(function(){console.log(2)}, 1000);
+setTimeout(function(){console.log(3)}, 1000);
